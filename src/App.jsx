ef1ec0e1750bb4ca812e5861a3b9b5b243ffa3e4
livferz <liviafernandesfaria@gmail.com>
2024-06-10 11:02:56 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import {Routes, Route} from "react-router-dom"  
 import Home from "./pages/Home";                 
-import Inicio from "./pages/inicio";
+import Inicio from "./pages/Inicio";
 
 const App = () => {
 const [color] = useState ("#eb568f")
